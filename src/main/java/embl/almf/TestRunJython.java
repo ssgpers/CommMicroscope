4b42a44005684a76d8f0a2169b0e38c54b63fa8a
Kota Miura <miura@embl.de>
2012-11-23 14:18:11 +0100

    test running jython script from java

---
+++ src/main/java/embl/almf/TestRunJython.java
@@ -0,0 +1,18 @@
+package embl.almf;
+
+import common.ScriptRunner;
+
+public class TestRunJython {
+	
+	/** for debugging
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String jyfilepath = "/Users/miura/Desktop/tmp/testjy.py";
+		ScriptRunner.runPY(jyfilepath, null);
+		
+	}	
+
+}
+
